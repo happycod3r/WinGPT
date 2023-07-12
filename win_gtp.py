dfe586cca4753ea852442190337d3edb79da90b9
happycod3r <happy2code0101@gmail.com>
2023-07-12 15:10:27 -0400

    [chore] Finished setting up converse()

--- win_gtp.py
+++ win_gtp.py
@@ -9,19 +9,61 @@ class WinGTP:
         self.engine = 'text-davinci-003'
         self.api_key_path = api_key_path
         self.api_key = self.getAPIKey(self.api_key_path)
+        self.api_base = openai.api_base
+        self.api_type = openai.api_type
+        self.api_version = openai.api_version
+        self.data_file = None
         self.request = 'What\'s todays date?'
         self.response = None
+        self.organization = openai.organization
+        self.user_defined_filename = None
         openai.api_key_path = self.api_key_path
-        # openai.api_key = self.api_key
-        
-    def getAPIKeyPath(self):
-        return self.api_key_path
+        openai.api_key = self.api_key
     
+    def setAPIBase(self, api_base):
+        self.api_base = api_base
+        openai.api_base = self.api_base
+        
+    def getAPIBase(self):
+        return openai.api_base
+    
+    def setAPIType(self, api_type):
+        self.api_type = api_type
+        openai.api_type = self.api_type
+        
+    def getAPIType(self):
+        return openai.api_type
+    
+    def setAPIVersion(self, api_version):
+        self.api_version = api_version
+        openai.api_version = self.api_version
+        
+    def getAPIVersion(self):
+        return openai.api_version
+    
+    def setOrganization(self, organization):
+        self.organization = organization
+        openai.organization = self.organization
+        
+    def getOrganization(self):
+        return openai.organization
+    
+    def setUserDefinedFileName(self, file_name):
+        self.user_defined_filename = file_name
+        
+    def getUserDefinedFileName(self):
+        return self.user_defined_filename
+        
     def setAPIKeyPath(self, api_key_path):
         self.api_key_path = api_key_path
+        openai.api_key_path = self.api_key_path
+        
+    def getAPIKeyPath(self):
+        return openai.api_key_path
         
     def setAPIKey(self, api_key):
         self.api_key = api_key
+        openai.api_key = self.api_key
         
     def getAPIKey(self, api_key_path):
         try:
@@ -43,6 +85,25 @@ class WinGTP:
     def getEngine(self):
         return self.engine
     
+    def setDataFile(self, jsonl_file_path):
+        self.data_file = file_obj = openai.File.create(
+            file=open(f"{jsonl_file_path}", 'rb'),
+            purpose='fine-tune',
+            model=self.engine,
+            api_key = self.api_key,
+            api_base = self.api_base,
+            api_type = self.api_type,
+            api_version = self.api_version,
+            organization = self.organization,
+            user_provided_filename = self.user_defined_filename,     
+        )
+        
+    def getDataFile(self):
+        return self.data_file 
+    
+    def readDataFile(self):
+        return None
+    
     def setRequest(self, request):
         self.request = request
         
@@ -62,43 +123,43 @@ class WinGTP:
         self.response_count = response_count
         
     def requestData(self):
-        response = openai.Completion.create(
-            engine=f"{self.engine}",                # The model being used.
-            prompt=f"{self.request}",               # The input.
-            max_tokens=self.response_token_limit,   # Max tokens allowed in each response.
-            n=self.response_count,                  # Number of responses 
-        )
-        self.response = response
+        if(self.data_file == None):
+            self.response = openai.Completion.create(
+                engine=f"{self.engine}",                # The model being used.
+                prompt=f"{self.request}",               # The input.
+                max_tokens=self.response_token_limit,   # Max tokens allowed in each response.
+                n=self.response_count,                  # Number of responses 
+            )
+        else:
+            self.response = openai.Completion.create(
+                engine=f"{self.engine}",                # The model being used.
+                prompt=f"{self.request}",               # The input.
+                max_tokens=self.response_token_limit,   # Max tokens allowed in each response.
+                n=self.response_count,                  # Number of responses 
+                files=[self.data_file.id]
+            )
         
     def getResponse(self):
         return self.response.choices[0].text.strip()
-    
-REQUEST = "How do you write a function in javascript?"
-API_KEY_PATH = './.api_key.conf'
-RESPONSE_TOKEN_LIMIT = 200
-RESPONSE_COUNT = 1
-ENGINE = 'text-davinci-003'
 
+    def converse(self):
+        while True:
+            user_prompt = input(':>>> ')
+            if user_prompt == 'exit':
+                print(f'Goodbye! ...')
+                sys.exit()
+            else:
+                REQUEST = str(user_prompt)
+                RESPONSE_TOKEN_LIMIT = 200
+                RESPONSE_COUNT = 1
+                ENGINE = 'text-davinci-003'
+                self.setResponseTokenLimit(RESPONSE_TOKEN_LIMIT)
+                self.setEngine(ENGINE)
+                self.setResponseCount(RESPONSE_COUNT)
+                self.setRequest(REQUEST)
+                self.requestData()
+                print(self.getResponse())
+                
+API_KEY_PATH = './.api_key.conf'
 newRequest = WinGTP(API_KEY_PATH)
-newRequest.setEngine(ENGINE)
-print("//////////////// WinGTP ////////////////")
-print("----------------------------------------")
-print("Engine:")
-print(newRequest.getEngine())
-newRequest.setResponseTokenLimit(RESPONSE_TOKEN_LIMIT)
-print("----------------------------------------")
-print("Response token limit:")
-print(newRequest.getResponseTokenLimit())
-newRequest.setResponseCount(RESPONSE_COUNT)
-print("----------------------------------------")
-print("Engine:")
-print(newRequest.getResponseCount())
-newRequest.setRequest(REQUEST)
-print("----------------------------------------")
-print("Request:")
-print(newRequest.getRequest())
-newRequest.requestData()
-print("----------------------------------------")
-print("Response:")
-print(newRequest.getResponse())
-print("\n")
+newRequest.converse()
