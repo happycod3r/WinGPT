ac122b3d4e5bc019c001a95d7ab3ce9efd90ec67
happycod3r <happy2code0101@gmail.com>
2023-07-12 03:03:29 -0400

    [chore]

--- api_key.py
+++
@@ -1,6 +0,0 @@
-# api-key.py
-
-# ** IMPORTANT **
-# This file should be added to the .gitignore for obvious security/privacy purposes.
-
-OPENAI_API_KEY = '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'
